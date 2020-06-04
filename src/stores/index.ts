bf8116f2ab4f27f40d8d433a8a3071a3384e713b
Ruslan <stonetiger1997@gmail.com>
2020-06-04 11:58:27 +0600

    function for update params

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,7 +2,6 @@ import { createContext } from "react";
 import { action, observable } from "mobx";
 import { category } from "../consts/category";
 import { WINDOW_SIZES } from "../consts/windowSize";
-import { Category } from "../interfaces/store";
 
 class PaginationStore {
   @observable public params = {
@@ -37,19 +36,23 @@ class PaginationStore {
     const lastElement = this.params.category.slice(-1)[0];
 
     if (windowSize <= 1200) {
-      this.params.limit = 4;
+      this.updateDataWithAction(() => {
+        this.params.limit = 4;
 
-      if (lastElement.id === this.params.activeCategoryId) {
-        this.params.category.shift();
-      }
-
-      this.setPage(this.params.activeCategoryId);
+        if (lastElement.id === this.params.activeCategoryId) {
+          this.params.category.shift();
+        }
+      });
     } else {
-      this.params.limit = 5;
-      this.setPage(this.params.activeCategoryId);
+      this.updateDataWithAction(() => {
+        this.params.limit = 5;
+      });
     }
+  };
 
-    this.params.windowWidth = windowSize;
+  @action updateDataWithAction = (action: () => void) => {
+    action();
+    this.setPage(this.params.activeCategoryId);
   };
 }
 
